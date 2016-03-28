9cbb91a9859ad7bef01cf9c034b5d0ccaa090f17
Darylcxz <dcxz96@live.com>
2016-03-28 14:48:39 +0800

    commited the wrong project folder lol

---
+++ Assets/Scripts/PlayerLogic/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody rb;
+    [SerializeField]float speed;
+    [SerializeField]float jumpspeed;
+    bool OnGround;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move(Input.GetAxisRaw("Horizontal"));
+	}
+
+    void Move(float h)
+    {
+        Vector3 movedir = Vector3.right * h * speed;
+        rb.velocity = movedir;
+    }
+}
