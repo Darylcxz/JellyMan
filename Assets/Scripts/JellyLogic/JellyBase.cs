9328edf8254f81b552be587a297f02e0abc138ef
Amadeus Chia <foodpunch@gmail.com>
2016-04-09 09:59:16 +0800

    Added Jelly Classes Added Jelly base class Testing out physics Made base template for jelly prefab

---
+++ Assets/Scripts/JellyLogic/JellyBase.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JellyBase : MonoBehaviour {
+
+	public enum JellyType //6-7 total
+    {
+        RED,            //makes surfaces bouncy
+        BLUE,           //makes them slippery, propels player
+        PURPLE,         //Magnetic, pulls player towards
+        YELLOW,         //Repulsion, pushes player away
+        BLACK,          //Strong acid, deletes blocks
+        WHITE,          //Cum, erases stuff
+
+    }
+    public JellyType JellyColor;
+    protected virtual void OnCollisionEnter()
+    {
+        //Might wanna check if it hits on a surface? Or do we do that via physics layering lol
+        JellyPower();           //Jelly power activates whenever it hits any surface whatsoever
+    }
+    protected virtual void JellyPower()
+    {
+
+    }
+}
+
+
